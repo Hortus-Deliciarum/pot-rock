d5b60766e338dd44e0d4f2c6a6d5b69439bf0c55
neum <elettrochuck@gmail.com>
2022-08-28 12:24:16 +0200

    REFACTOR

--- core/serial_listen.py
+++ core/serial_listen.py
@@ -1,35 +1,56 @@
 #!/usr/bin/env python3
+#pylint: disable=too-many-function-args
+#pylint: disable=missing-module-docstring
+#pylint: disable=missing-function-docstring
+#pylint: disable=global-statement
 
+from pathlib import Path
 import serial
+import toml
+from debugs import debug
 from pythonosc import udp_client
 
-LOCALHOST = "127.0.0.1"
-PORT = 10000
+MAINPATH = Path(__file__).parent.absolute()
+CONFIG_PATH = MAINPATH / Path('../config/config.toml')
+IP, OUTPORT, DEBUG, ADDRESS = [None] * 4
+SERIAL = serial.Serial('/dev/ttyS1', 115200)  # Start serial communication
 
-ser = serial.Serial('/dev/ttyS1', 115200)  # Start serial communication
 
+def init_config():
+    """config global labels"""
+    global IP, OUTPORT, DEBUG, ADDRESS
+
+    with open(CONFIG_PATH, 'r', encoding='utf-8') as c_file:
+        t_data = toml.load(c_file)
+
+    DEBUG = t_data['debug']['DEBUG']
+    IP = t_data['network']['IP']
+    OUTPORT = t_data['network']['OUT_PORT']
+    ADDRESS = t_data['address']['ADDRESS']
+    debug(DEBUG, "[ VALUES ]\t", DEBUG, IP, OUTPORT, ADDRESS)
 
 def parse_msg(msg):
-    """parse serial message"""
+    init_config()
     address, datum = msg.split(' ')[:2]
     datatyped = None
 
     try:
-        datatyped = int(datum.strip(None))
+        datatyped = float(datum.strip(None))
     except ValueError:
-        try:
-            datatyped = float(datum.strip(None))
-        except ValueError:
-            datatyped = datum
+        datatyped = datum
+
     print(address, datatyped)
     return (address, datatyped)
 
+def send_msg(_client, _addr, _val):
+    if _addr in ADDRESS:
+        _client.send_message(_addr, _val)
 
 if __name__ == '__main__':
-    client = udp_client.SimpleUDPClient(LOCALHOST, PORT)
+    client = udp_client.SimpleUDPClient(IP, OUTPORT)
 
     while True:
-        data = ser.readline()
+        data = SERIAL.readline()
         s = data.decode('ascii')
         addr, value = parse_msg(s)
-        client.send_message(addr, value)
+        send_msg(client, addr, value)
